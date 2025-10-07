cb114914fa432c9b1156416a77bed517a63ed381
dorianBess <dorianbessiere2@gmail.com>
2025-10-07 16:16:28 +0200

    Update notes.py

--- src/mon_premier_projet/notes/notes.py
+++ src/mon_premier_projet/notes/notes.py
@@ -1,13 +1,13 @@
 notes = []
 
-def add_note(note : int ,movie : string, all_movies : []):
-    all_movies[movie] = note
+def add_note(note : int ,movie : string):
+    notes[movie] = note
 
-def get_highest_note(all_movies : []):
-    high_note = allMovies[0]
-    highest_movie = all_movies[0]
-    for movie in allMovies:
-        if all_movies[movie] >= high_note:
-            high_note = all_movies[movie]
+def get_highest_note():
+    high_note = notes[0]
+    highest_movie = notes[0]
+    for movie in notes:
+        if notes[movie] >= high_note:
+            high_note = notes[movie]
             highest_movie = movie
     return highest_movie + " " + highest_note
